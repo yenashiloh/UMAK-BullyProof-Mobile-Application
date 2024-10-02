37278e27bcb6682d5313dee15cddf2125f3ff2b1
yenashiloh <shiloheugenio21@gmail.com>
2024-10-02 10:52:20 +0800

    config updated

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
-const url = 'http://192.168.0.38:3000/';
+const url = 'http://52.195.171.20/';
 const registration = "${url}registration";
 const login = "${url}login";
 const report = "${url}reports/submit";
