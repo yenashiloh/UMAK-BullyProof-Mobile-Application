2f7effec77bd41c855bb2d6cefb3b20fb8bd2a50
CodeAce36 <paulangelo.derige@gmail.com>
2025-05-25 17:33:17 +0800

    05-25-2025

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
-// const url = 'http://54.169.171.26/';
-const url = 'http://192.168.1.12:3000/';
+const url = 'http://13.215.207.69/';
+// const url = 'http://192.168.1.12:3000/';
 const registration = "${url}registration";
 const login = "${url}login";
 const report = "${url}reports/submit";
